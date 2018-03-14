eb4f603e71449bcb819a6b39c52f9c1beccc9d45
Glenn-David Daniel <d2rd@outlook.com>
2018-03-14 13:23:16 -0700

    Added routes skeleton to routes.js

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,3 +1,14 @@
 module.exports = (app) => {
   //add your new routes here
+
+  Router.____('/', function(req, res) {}); //login page. Submits a POST request to find a user in the DB and send that response back to the client. 
+
+
+  Router.____('/create-user', function(req, res) {}); // create user form. 
+  
+  Router.____('/posts', function(req, res) {}); // display all of the blog posts in the DB. 
+
+  Router.____(/posts:id, function(req, res) {}); // navigate to the single blog post page. 
+  
+  Router.____(/new-post, function(req, res) {}); // create a new blog post. 
 };
