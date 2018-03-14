99d4d79f0d1401e934387a7f0392f90f60589562
Mephestys <mephestys@gmail.com>
2018-03-14 16:43:40 -0600

    Established new-user route, model, and controller. - Saves new users to database.

--- api/controllers/userControllers.js
+++ api/controllers/userControllers.js
@@ -1,19 +1,18 @@
 const User = require('../models/userModels');
 
-function newUser(body) {
-  const { userName, password } = body;
-  const user = new User(userName, password); 
-  return user.save()
-  .then(newdude => {
-    res.status(201).json(newdude);
-  })
-  .catch(err => {
-    res.status(400).json({ errorMessage: err }).end();
-  });
+function newUser(req, res) {
+  const user = new User(req.body);
+  user.save()
+    .then(newdude => {
+      res.status(201).json(newdude);
+    })
+    .catch(err => {
+      res.status(400).json({ errorMessage: err }).end();
+    });
 }
 
 // function validateUser() {
 
 // }
 
-module.exports = { newUser };
+module.exports = newUser;
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,12 +1,7 @@
 const newUser = require('../controllers/userControllers');
 
-
 module.exports = (app) => {
   app.post('/new-user', (req, res) => {
-    newUser(req.body)
-    .then(user => res.send(user))
-    .catch(err => res.send(err));
+    newUser(req, res);
   });
-  
 };
-
