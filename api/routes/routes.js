850ccf5025f7c943314535dad6a9103280df080c
Glenn-David Daniel <d2rd@outlook.com>
2018-03-14 15:44:27 -0700

    WIP routes.js

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,14 +1,18 @@
 module.exports = (app) => {
   //add your new routes here
 
-  Router.____('/', function(req, res) {}); //login page. Submits a POST request to find a user in the DB and send that response back to the client. 
+  Router.get('/', function(req, res) {}); //login page. Submits a POST request to find a user in the DB and send that response back to the client. 
 
 
   Router.____('/create-user', function(req, res) {}); // create user form. 
   
-  Router.____('/posts', function(req, res) {}); // display all of the blog posts in the DB. 
+  Router.get('/posts', function(req, res) {
 
-  Router.____(/posts:id, function(req, res) {}); // navigate to the single blog post page. 
+  }); // display all of the blog posts in the DB. 
+
+  Router.get(/posts:id, function(req, res) {
+    git
+  }); // navigate to the single blog post page. 
   
   Router.____(/new-post, function(req, res) {}); // create a new blog post. 
 };
