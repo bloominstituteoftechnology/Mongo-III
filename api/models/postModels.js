57974d2d36059a58a479ce31d12b4afa834b9382
Christopher Beards <beardscm@gmail.com>
2018-03-15 09:33:16 -0400

    postModel Created

---
+++ api/models/postModels.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const User = require('./userModels');
+
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const PostSchema = Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: ObjectId,
+    ref: 'User',
+  },
+  comments: [CommentSchema],
+});
+
+module.exports = mongoose.model('Post', PostSchema);
