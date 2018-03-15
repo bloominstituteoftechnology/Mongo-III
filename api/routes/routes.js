22cfc641250505328e7e6255c3cdc5a25fc6e922
german gonzalez <gcomlnk@gmail.com>
2018-03-15 01:38:55 -0400

    using populate to get the username from posts

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -48,21 +48,20 @@ module.exports = (appRouter) => {
 
     appRouter.get('/posts/:id', (req, res) => {
         PostModel.findById(req.params.id)
-            .then(post => res.status(200).send(post))
-            .catch(err => res.status(400).send({
-                error: `The information could not be reached /posts/:id. ${err}`
-            }));
+            .populate('author')
+            .exec((err, post) => {
+                res.status(200).send(post)
+            });
     });
 
     appRouter.put('/posts/:id', (req, res) => {
         console.log('req.body:::', req.body);
 
         PostModel.findByIdAndUpdate(req.params.id, req.body, { runValidators: true })
-            .then(post => res.status(200).send(post))
+            .then(post => res.status(201).send(post))
             .catch(err => res.status(400).send({
                 error: `The information could not be reached /posts/:id. ${err}`
             }));
     });
-
 
 };
