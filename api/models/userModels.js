0a0020f1016533a7533bfd898f659bfe1b7d63a2
Christopher Beards <beardscm@gmail.com>
2018-03-15 09:22:28 -0400

    Environment Established-userModel Created

---
+++ api/models/userModels.js
@@ -0,0 +1,15 @@
+const Mongoose = require('mongoose');
+
+const UserSchema = new Mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = Mongoose.model('User', UserSchema);
