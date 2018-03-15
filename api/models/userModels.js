d15d32ec8f375e6437ff329b4ffe4ece7cc18015
german gonzalez <gcomlnk@gmail.com>
2018-03-14 21:17:58 -0400

    creating model for user

---
+++ api/models/userModels.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, 'Please provide username.']
+    },
+    password: {
+        type: String,
+        min: [6, 'Min 6 Characters.'],
+        max: [18, 'Max 18 Characters.'],
+    },
+    createdOn: {
+        type: String,
+        required: true,
+        default: new Date(),
+    }
+});
+
+
+module.exports = mongoose.model('users', UserSchema);
