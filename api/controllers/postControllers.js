f97b065fd9add7f091b1f5400eeba2415ca5af74
Christopher Beards <beardscm@gmail.com>
2018-03-15 17:28:33 -0400

    findById Created but Not Working

--- api/controllers/postControllers.js
+++ api/controllers/postControllers.js
@@ -18,7 +18,7 @@ const getPosts = (req, res) => {
     .select('title')
     .then(posts => {
       if (!posts) {
-        res.status(500).json({ message: 'There was an error!', error: err })
+        res.status(404).json({ message: 'No such post found!' })
       } else {
         res.status(200).json(posts);
       }
@@ -28,7 +28,24 @@ const getPosts = (req, res) => {
     });
 };
 
+const findById = (req, res) => {
+  const { id } = req.params;
+  Post.findById(id)
+    .populate('author')
+    .then(post => {
+      if (!post) {
+        res.status(404).json({ message: 'No such post found!' });
+      } else {
+        res.status(200).json(post);
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'There was an error!', error: err });
+    });
+}
+
 module.exports = {
   createPost,
-  getPosts
+  getPosts,
+  findById
 };
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -4,4 +4,5 @@ module.exports = (app) => {
   //add your new routes here
   app.route('/new-post').post(postControllers.createPost);
   app.route('/posts').post(postControllers.getPosts);
+  app.route('/posts:id').post(postControllers.findById);
 };
