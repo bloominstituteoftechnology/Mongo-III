d536365f48a0e750aba055cf574703ece43bada3
german gonzalez <gcomlnk@gmail.com>
2018-03-14 21:19:09 -0400

    creating get / and post /new-user routes

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,3 +1,24 @@
-module.exports = (app) => {
-  //add your new routes here
+const express = require('express');
+const UserModel = require('../models/userModels'); //mongoose instance
+const userRouter = express.Router();
+
+module.exports = (userRouter) => {
+
+    userRouter.get('/', (req, res) => {
+        UserModel.find({})
+            .then(user => res.status(200).send(user))
+            .catch(err => res.status(400).send({
+                error: `The information could not be reached. ${err}`
+            }));
+    });
+
+    userRouter.post('/new-user', (req, res) => {
+        const user = new UserModel(req.body);
+        user.save()
+            .then(usr => res.status(201).send(usr))
+            .catch(err => {
+                res.status(500).send({ error: "Something went wrong saving your user information", info: err});
+            });
+    });
+
 };
